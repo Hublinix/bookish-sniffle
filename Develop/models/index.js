3351a50478e944e09ebda0f9121c96b2b60616e2
Kachi <kachinwosu1999@gmail.com>
2024-08-27 13:05:08 -0500

    Bookish

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,15 +3,28 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { model } = require('../config/connection');
 
 // Products belongsTo Category
-
+Product.belongesToMany(Category);
 // Categories have many Products
-
+Category.belongesToMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongesToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'types_of_products'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongesToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'list_of_products'
+});
 module.exports = {
   Product,
   Category,
